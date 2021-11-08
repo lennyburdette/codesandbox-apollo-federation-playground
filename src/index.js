9baf631256303a41cdb6f0f732287021975ecb5c
Lenny Burdette <lenny@apollographql.com>
2021-11-08 14:54:08 -0800

    upgrade dependencies

--- src/index.js
+++ src/index.js
@@ -2,41 +2,45 @@ const { ApolloServer } = require("apollo-server");
 const { ApolloGateway, LocalGraphQLDataSource } = require("@apollo/gateway");
 const { buildFederatedSchema } = require("@apollo/federation");
 const walk = require("walk-sync");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
 
 const localServiceList = walk(`${__dirname}/services`, {
-  directories: false
-}).map(file => require(`${__dirname}/services/${file}`));
+  directories: false,
+}).map((file) => require(`${__dirname}/services/${file}`));
 
 const gateway = new ApolloGateway({
   debug: true,
   localServiceList,
-  buildService: service => {
+  buildService: (service) => {
     return new LocalGraphQLDataSource(buildFederatedSchema([service]));
-  }
+  },
 });
 
 const server = new ApolloServer({
   gateway,
-  subscriptions: false,
-  playground: {
-    tabs: [
-      {
-        endpoint: process.env.SANDBOX_URL,
-        query: `query {
-  me {
-    username
-    reviews {
-      body
-      product {
-        name
-        upc
-      }
-    }
-  }
-}`
-      }
-    ]
-  }
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground({
+      tabs: [
+        {
+          endpoint: process.env.SANDBOX_URL,
+          query: `query {
+            me {
+              username
+              reviews {
+                body
+                product {
+                  name
+                  upc
+                }
+              }
+            }
+          }`,
+        },
+      ],
+    }),
+  ],
 });
 
 server.listen(8080).then(({ url }) => {
